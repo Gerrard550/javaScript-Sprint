96374d5f54b5a87a3c343568eb58ed3524a3dadf
Gerrard550 <privatesystem07@gmail.com>
2024-10-09 16:22:27 -0700

    values and variables

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log('Biodun')
-console.log('learning')
+console.log("Biodun");
+console.log("learning");
+console.log("Liverpool");
+console.log("David vs Goliath");
+
+let battlename = "David vs Goliath";
+console.log("battlename");
+
+let clubname = "Liverpool";
+console.log(clubname);
+
+let statusquo = "learning";
+console.log(statusquo);
+
+let stadium = "Wembley";
+console.log(stadium);
+console.log(stadium);
+console.log(stadium);
+console.log(stadium);
+console.log(stadium);
