a1294d4ae76aa5ec6a084df2c71ba2dd924ab824
Gerrard550 <privatesystem07@gmail.com>
2024-10-09 17:57:49 -0700

    assignments

---
+++ assignments.js
@@ -0,0 +1,9 @@
+let country = "Nigeria";
+let continent = "Africa";
+let population = "160 000 000";
+let states = "36";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(states);
