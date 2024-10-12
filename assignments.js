39f1914ac54855f770c21fe8204752083ddc5ff3
Gerrard550 <privatesystem07@gmail.com>
2024-10-11 21:11:12 -0700

    challenge1

--- assignments.js
+++ assignments.js
@@ -1,48 +1,16 @@
-let country = "Nigeria";
-let continent = "Africa";
-let population = "160 000 000";
-let states = "36";
+// DATA 1
+let markWeight = 78;
+let markheight = 1.69;
+let johnweight = 92;
+let johnheight = 1.95;
 
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(states);
-console.log(typeof states);
+console.log(markWeight);
+console.log(markheight);
+console.log(johnweight);
+console.log(johnheight);
 
-console.log(typeof "Nigeria");
-console.log(typeof "Africa");
-console.log(typeof "population");
-console.log(typeof "states");
+let markBMI = 78 / (1.69 * 1.69);
+let johmBMI = 92 / (1.95 * 1.95);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 66);
-console.log(typeof "Biodun");
-
-javascriptIsFun = "YES";
-
-// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
-
-// Log the types of isIsland, population, country and language to the console.
-
-// Solution
-
-let isIsland = 40;
-console.log(isIsland);
-console.log(typeof isIsland > 40);
-let language = "Yoruba";
-console.log(language);
-
-const birthYear = 1990;
-const age = 34;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-
-let countryPopulation = x;
-splitPopulation = x / 2;
-console.log(splitPopulation);
-x++;
-console.log(x++);
+console.log(markBMI);
+console.log(johmBMI);
