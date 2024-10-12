8396393087f1eecd1b78272a48a2a551961ccbc4
Gerrard550 <privatesystem07@gmail.com>
2024-10-11 21:39:14 -0700

    test data2

--- assignments.js
+++ assignments.js
@@ -1,19 +1,25 @@
 // DATA 1
-const markWeight = 78;
-const markheight = 1.69;
-const johnweight = 92;
-const johnheight = 1.95;
+// let markWeight = 78;
+// let markheight = 1.69;
+// let johnweight = 92;
+// let johnheight = 1.95;
+
+const markWeight = 95;
+const markheight = 1.88;
+const johnweight = 85;
+const johnheight = 1.76;
 
 console.log(markWeight);
 console.log(markheight);
 console.log(johnweight);
 console.log(johnheight);
 
-let markBMI = 78 / (1.69 * 1.69);
-let johmBMI = 92 / (1.95 * 1.95);
+let markBMI = 95 / (1.88 * 1.88);
+let johmBMI = 85 / (1.76 * 1.76);
 
 console.log(markBMI);
 console.log(johmBMI);
 
 markhigherBMI = markBMI > johmBMI;
 console.log(markhigherBMI);
+console.log(markBMI, johmBMI, markhigherBMI);
