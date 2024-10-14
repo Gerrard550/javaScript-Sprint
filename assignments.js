26aae574fd15c22cc74d094b00847c7143a1b0b9
Gerrard550 <privatesystem07@gmail.com>
2024-10-14 16:42:26 -0700

    equality operators

--- assignments.js
+++ assignments.js
@@ -9,29 +9,29 @@ let johnheight = 1.95;
 // const johnweight = 85;
 // const johnheight = 1.76;
 
-console.log(markWeight);
-console.log(markheight);
-console.log(johnweight);
-console.log(johnheight);
+// console.log(markWeight);
+// console.log(markheight);
+// console.log(johnweight);
+// console.log(johnheight);
 
-let markBMI = 78 / (1.69 * 1.69);
-let johmBMI = 92 / (1.95 * 1.95);
+// let markBMI = 78 / (1.69 * 1.69);
+// let johmBMI = 92 / (1.95 * 1.95);
 
-console.log(markBMI, johmBMI);
+// console.log(markBMI, johmBMI);
 
-if (markBMI > johmBMI) {
-  console.log(`Mark's BMI (${markBMI}) is higher than John's BMI(${johmBMI})`);
-} else {
-  console.log(`John's BMI(${johmBMI}) is higher than Mark's BMI(${markBMI})!`);
-}
+// if (markBMI > johmBMI) {
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI(${johmBMI})`);
+// } else {
+//   console.log(`John's BMI(${johmBMI}) is higher than Mark's BMI(${markBMI})!`);
+// }
 
-// MUSA
-const occupation = "CryptoCurrency";
-const nickName = "Musadgreat1";
-const tokenMining = "hamsterKombat";
-const airdropStart = "2018";
-const airdropYear = "2024";
-const Musadgreat1 = `${nickName} Is a ${occupation} Lord who has been trading ${tokenMining} for ${
-  airdropYear - airdropStart
-} years.`;
-console.log(Musadgreat1);
+// // MUSA
+// const occupation = "CryptoCurrency";
+// const nickName = "Musadgreat1";
+// const tokenMining = "hamsterKombat";
+// const airdropStart = "2018";
+// const airdropYear = "2024";
+// const Musadgreat1 = `${nickName} Is a ${occupation} Lord who has been trading ${tokenMining} for ${
+//   airdropYear - airdropStart
+// } years.`;
+// console.log(Musadgreat1);
--- script.js
+++ script.js
@@ -114,13 +114,52 @@
 
 // TYPE CONVERSION AND COERCION
 
-// type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// // type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("James"));
-console.log(typeof NaN);
+// console.log(Number("James"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
+// // type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("I am " + "23" + " years old");
+
+// console.log("23" - "10" - "3");
+// console.log("20" * "5");
+// console.log("20" / "5");
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// falsy
+
+// 3 falsy values: 0, ''; undefined, null, Nan
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("James"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// Equality Operators
+const age = 18;
+if (age === 18) console.log("You are now an adult");
