2b34bc3116af9ccd2ea5f57f9bfc7709e2d79ab6
Gerrard550 <privatesystem07@gmail.com>
2024-10-18 23:41:49 -0700

    Switch statement

--- script.js
+++ script.js
@@ -210,3 +210,43 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive...");
 }
+
+// SWITCH STATEMENT
+const day = "monday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesdsy":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record Videos");
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend 😎");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record Videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend 😎");
+} else {
+  console.log("Not a good day");
+}
