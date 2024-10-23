aced57360bd85e4d58192d34a6515df2ff8fd03b
Gerrard550 <privatesystem07@gmail.com>
2024-10-22 18:44:46 -0700

    functions

--- script2.js
+++ script2.js
@@ -1,10 +1,33 @@
-"use strict";
+// "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive !");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive !");
 
-// const interface = "Audio";
-const private = "676";
+// // const interface = "Audio";
+// const private = "676";
+
+// FUNCTIONS
+function logger() {
+  console.log("My name is Gerrard");
+}
+
+function job() {
+  console.log("I write codes for a living");
+}
+
+// calling/ running/ invoing functions
+logger();
+job();
+
+function fruitProcessor(apples, oranges, bananas) {
+  console.log(apples, oranges, bananas);
+  const juice = `Juice with ${apples} apples, ${oranges} oranges and ${bananas} bananas.`;
+  return juice;
+}
+
+const fruitJuice = fruitProcessor(4, 4, 2);
+console.log(fruitJuice);
+console.log(fruitProcessor(4, 4, 2));
