0225a26aa39ab26a798fe75a7355952ecd3ef8b4
Gerrard550 <privatesystem07@gmail.com>
2024-10-27 23:59:43 -0700

    array update

--- script2.js
+++ script2.js
@@ -122,3 +122,6 @@ const years = new Array(1991, 1984, 2008, 2020);
 console.log(friends[0]);
 console.log(friends[3]);
 console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
