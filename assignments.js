85d37bc2fc8d9da21551ce3066c85491c04b69cd
Gerrard550 <privatesystem07@gmail.com>
2024-10-29 17:27:53 -0700

    challenge #6

--- assignments.js
+++ assignments.js
@@ -118,3 +118,10 @@ let johnheight = 1.95;
 // scoreKoalas = calcAverage(23, 34, 27);
 // console.log(scoreDolphins, scoreKoalas);
 // checkWinner(scoreDolphins, scoreKoalas);
+
+// CHALLENGE #6
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const calcTip = (bill) => (bill) =>
+  50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
