f147a1f24ea6c8831e383788b39df3c890fd8466
Gerrard550 <privatesystem07@gmail.com>
2024-11-07 03:56:32 -0800

    challenge #6 completed

--- assignments.js
+++ assignments.js
@@ -128,4 +128,5 @@ const calcTip = function (bill) {
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
