90e6a5ec7a34452b3499be2bc0620f3ccafcd3ac
Gerrard550 <privatesystem07@gmail.com>
2024-11-23 21:41:51 -0800

    selecting manipulating elements

--- assignments.js
+++ assignments.js
@@ -339,16 +339,16 @@
 
 // CHALLENGE #9
 
-const data1 = [22, 21, 25];
-const data2 = [12, 5, -5, 0, 4];
+// const data1 = [22, 21, 25];
+// const data2 = [12, 5, -5, 0, 4];
 
-console.log(`...${data1[0]}▫C...${data1[1]}▫C...${data1[2]}▫C...`);
+// console.log(`...${data1[0]}▫C...${data1[1]}▫C...${data1[2]}▫C...`);
 
-const printForecast = function (arr) {
-  let str = '';
-  for (let i = 0; i < arr.length; i++) {
-    str += `${arr[i]}▫C in ${i + 1} days ...`;
-  }
-  console.log('...' + str);
-};
-printForecast(data1);
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}▫C in ${i + 1} days ...`;
+//   }
+//   console.log('...' + str);
+// };
+// printForecast(data1);
