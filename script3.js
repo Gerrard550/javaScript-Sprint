3d158c37e33541d10fb716c6e76e939dd1fc4075
Gerrard <privatesystem07@gmail.com>
2024-12-17 04:48:53 -0800

    no number

--- script3.js
+++ script3.js
@@ -10,6 +10,12 @@
 // document.querySelector('.guess').value = 44;
 // console.log(document.querySelector('.guess').value);
 
-document.querySelector('.check').addEventListener('click' , function() {
-    console.log(document.querySelector('.guess').value);
+document.querySelector('.btn-check').addEventListener('click' , function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!'
+    }
+    
 });
