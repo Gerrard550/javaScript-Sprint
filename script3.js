ac9eb87fecc9b4ac2d146960e3556ba943d16874
Gerrard <privatesystem07@gmail.com>
2024-12-18 18:19:43 -0800

    again!!

--- script3.js
+++ script3.js
@@ -36,13 +36,18 @@ document.querySelector('.btn-check').addEventListener('click' , function() {
 
         // When guess is too high
     } else if (guess > secretNumber) {
+        if(score > 1) {
             document.querySelector('.message').textContent = '📈 Too high!';
             score--;
             document.querySelector('.score').textContent = score;
 
         } else {
             document.querySelector('.message').textContent = '😭You lost the game!'
+            document.querySelector('.score').textContent = 0;
         }
+
+        // When guess is too low
+
     
     } else if (guess < secretNumber) {
         if(score > 1) {
@@ -70,4 +75,4 @@ document.querySelector('.btn-again').addEventListener('click', function() {
 
     document.querySelector('body').style.backgroundColor = ' #dd8a0e';
     document.querySelector('.number').style.width = '100px';
-})
+});
